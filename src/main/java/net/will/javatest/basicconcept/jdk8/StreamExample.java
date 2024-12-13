69c713029d75f81ef70c41daea074726bacd38ec
Will <yzj_000@163.com>
2024-12-13 19:41:20 +0800

    A bit more tests of JDK 1.8 features

--- src/main/java/net/will/javatest/basicconcept/jdk8/StreamExample.java
+++ src/main/java/net/will/javatest/basicconcept/jdk8/StreamExample.java
@@ -7,18 +7,48 @@ import java.util.stream.Stream;
 public class StreamExample {
 
     public static void main(String[] args) {
+        sequentialStream();
+        System.out.println("==========");
+        parallelStream();
+        System.out.println("==========");
+
+        interateOnStaticMethod();
+        interateOnInstanceMethod();
+    }
+
+    private static void sequentialStream() {
         List<Integer> myList = new ArrayList<>();
         for(int i=0; i<120; i++) myList.add(i);
-        
-        //sequential stream
+
         Stream<Integer> sequentialStream = myList.stream();
         Stream<Integer> highNumsSeq = sequentialStream.filter(p -> p > 90);
         highNumsSeq.forEach(p -> System.out.println("High Nums sequential="+p));
-        
-        //parallel stream
+    }
+
+    private static void parallelStream() {
+        List<Integer> myList = new ArrayList<>();
+        for(int i=0; i<120; i++) myList.add(i);
+
         Stream<Integer> parallelStream = myList.parallelStream();
         Stream<Integer> highNums = parallelStream.filter(p -> p > 90);
         highNums.forEach(p -> System.out.println("High Nums parallel="+p));
+    }
+
+    private static void interateOnStaticMethod() {
+        List.of(1, 2, 3, 4, 5).forEach(StreamExample::staticMethod);
+    }
+
+    private static void interateOnInstanceMethod() {
+        StreamExample instance = new StreamExample();
+        List.of(1, 2, 3, 4, 5).forEach(instance::instanceMethod);
+    }
+
+    private static void staticMethod(Integer obj) {
+        System.out.println("staticMethod(): " + obj);
+    }
+
+    private void instanceMethod(Integer obj) {
+        System.out.println("instanceMethod(): " + obj);
     }
 }
 
